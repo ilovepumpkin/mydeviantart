5ee7c157017a0ee6afd24c1fc0e6e7b18eca623d
shenrui <shenrui@cn.ibm.com>
2017-04-06 16:45:04 +0800

    able to show images in waterfall mode

--- pages/gallery/gallery.js
+++ pages/gallery/gallery.js
@@ -1,24 +1,94 @@
 var dA = require('../../utils/deviantArt.js')
 
 var app = getApp()
-Page({
-	data: {
-		deviations: []
-	},
-	onLoad: function() {
-		// dA.fetchAccessToken()
-		var self = this
-		dA.getAll({
-			offset: 0
-		}).then(function(deviations) {
-			console.log(deviations)
-			self.setData({
-				deviations: deviations
-			})
-		});
 
-		dA.getComments("245372DE-D6F8-A2EB-CA61-BF3A76C5AFAC").then(function(comments) {
-			console.log(comments)
-		})
-	}
+var col1H = 0;
+var col2H = 0;
+
+Page({
+    data: {
+        images: [],
+        scrollH: 0,
+        imgWidth: 0,
+        loadingCount: 0,
+        col1: [],
+        col2: []
+    },
+    onImageLoad: function(e) {
+        let imageId = e.currentTarget.id;
+        let oImgW = e.detail.width; 
+        let oImgH = e.detail.height;
+        let imgWidth = this.data.imgWidth;
+        let scale = imgWidth / oImgW; 
+        let imgHeight = oImgH * scale; 
+
+        let images = this.data.images;
+        let imageObj = null;
+
+        for (let i = 0; i < images.length; i++) {
+            let img = images[i];
+            if (img.id === imageId) {
+                imageObj = img;
+                break;
+            }
+        }
+
+        imageObj.height = imgHeight;
+
+        let loadingCount = this.data.loadingCount - 1;
+        let col1 = this.data.col1;
+        let col2 = this.data.col2;
+
+        if (col1H <= col2H) {
+            col1H += imgHeight;
+            col1.push(imageObj);
+        } else {
+            col2H += imgHeight;
+            col2.push(imageObj);
+        }
+
+        let data = {
+            loadingCount: loadingCount,
+            col1: col1,
+            col2: col2
+        };
+
+        if (!loadingCount) {
+            data.images = [];
+        }
+
+        this.setData(data);
+    },
+    loadImages:function(){
+        var self = this
+        dA.getAll({
+            offset: 0
+        }).then(function(deviations) {
+            console.log(deviations);
+            var images = deviations.map(d => {
+                return { "pic": d.preview.src, "id": d.deviationid }
+            });
+            self.setData({
+                images: images,
+                loadingCount: images.length
+            });
+        });
+    },
+    onLoad:function(){
+        wx.getSystemInfo({
+            success: (res) => {
+                let ww = res.windowWidth;
+                let wh = res.windowHeight;
+                let imgWidth = ww * 0.48;
+                let scrollH = wh;
+
+                this.setData({
+                    scrollH: scrollH,
+                    imgWidth: imgWidth
+                });
+
+                this.loadImages();
+            }
+        })
+    }
 })
