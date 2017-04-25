b3d34cf7610478dcf34f4e7eae5c79326b4315af
shenrui <ilovepumpkin@126.com>
2017-04-25 19:56:26 +0800

    update intro page to use util.calImageSize

--- pages/intro/intro.js
+++ pages/intro/intro.js
@@ -1,66 +1,48 @@
-import { KEY_INTRO_READ } from '../../constants/constants'
+import {
+	KEY_INTRO_READ
+} from '../../constants/constants'
+import util from '../../utils/util'
 
 var app = getApp()
 Page({
-    data: {
-        slides: []
-    },
-    onLoad: function() {
-        this.setData(Object.assign({}, this.data, app.globalData))
+	data: {
+		slides: []
+	},
+	onLoad: function() {
+		this.setData(Object.assign({}, this.data, app.globalData))
 
-        const slides = [{
-            image: "intro_gallery.jpg",
-            text: "下拉刷新，上拉加载"
-        }, {
-            image: "intro_image.jpg",
-            text: "点缩略图，浏览大图"
-        }, {
-            image: "intro_share.jpg",
-            text: "点右上角，轻松转发"
-        }, {
-            image: "intro_info.jpg",
-            text: "左右滑动，查看详情"
-        }, {
-            image: "intro_author.jpg",
-            text: "点点点”他“，了解作者"
-        }]
+		const slides = [{
+			image: "intro_gallery.jpg",
+			text: "下拉刷新，上拉加载"
+		}, {
+			image: "intro_image.jpg",
+			text: "点缩略图，浏览大图"
+		}, {
+			image: "intro_share.jpg",
+			text: "点右上角，轻松转发"
+		}, {
+			image: "intro_info.jpg",
+			text: "左右滑动，查看详情"
+		}, {
+			image: "intro_author.jpg",
+			text: "点点点”他“，了解作者"
+		}]
 
+		//calculate the best image widht and height
+		let [imgWidth, imgHeight] = util.calImageSize(375, 600, this.data.winWidth * 0.8, this.data.winHeight * 0.8);
 
-        this.setData({
-            slides
-        })
+		this.setData({
+			slides,
+			imgWidth,
+			imgHeight
+		})
 
-        //calculate the best image widht and height
-        this.calImageSize();
-    },
-    calImageSize: function() {
-        const winWidth = this.data.winWidth;
-        const winHeight = this.data.winHeight;
+	},
+	enter: function() {
+		wx.setStorageSync(KEY_INTRO_READ, true)
 
-        const origImgWidth = 375
-        const origImgHeight = 600
-
-        let imgWidth = 0
-        let imgHeight = 0
-
-        if (origImgHeight > (winHeight - 70)) {
-            imgHeight = winHeight * 0.8
-            imgWidth = imgHeight * origImgWidth / origImgHeight
-        } else {
-            imgWidth = winWidth * 0.8
-            imgHeight = imgWidth * origImgHeight / origImgWidth
-        }
-
-        this.setData({
-            imgWidth,
-            imgHeight
-        })
-    },
-    enter: function() {
-        wx.setStorageSync(KEY_INTRO_READ, true)
-
-        wx.switchTab({
-            url: "/pages/gallery/gallery"
-        })
-    }
+		wx.switchTab({
+			url: "/pages/gallery/gallery"
+		})
+	}
 })
