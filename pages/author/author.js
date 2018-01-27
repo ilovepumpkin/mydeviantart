bc954a7fea4b70b12341cae5d3e4a0703661142c
shenrui <ilovepumpkin@126.com>
2018-01-27 22:11:32 +0800

    adding Add Author button

--- pages/author/author.js
+++ pages/author/author.js
@@ -13,6 +13,47 @@ Page({
   bindPickerChange:function(e){
 
   },
+  startEdit: function() {
+    this.setData({
+      isEdit: true
+    })
+  },
+  cancelChangeUser: function() {
+    this.setData({
+      username: "",
+      isEdit: false
+    })
+  },
+  saveUser: function() {
+    const username = this.data.username
+    if (username === util.getCurrentUser()) {
+      this.setData({
+        isEdit: false
+      })
+      return;
+    }
+    dA.whoami(username).then(resp => {
+      if (resp.error_description === "user not found." || resp.real_name === "") {
+        wx.showToast({
+          title: "账号[" + username + "]不存在！",
+          duration: 1500
+        })
+      } else {
+        addAuthor(username,resp.real_name,resp.user.usericon)
+        // this.clearStatsInfo()
+        this.onLoad()
+        this.setData({
+          isEdit: false
+        })
+      }
+    })
+  },
+  onUsernameInput: function(e) {
+    const username = e.detail.value
+    this.setData({
+      username
+    })
+  },
   onLoad: function() {
 
     let authors=getAuthors();
